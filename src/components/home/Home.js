5d687c10c0edd6dbfac32c5ea976ff3b22bc425b
Landon Morgan <morganlandon@gmail.com>
2020-02-24 15:51:32 -0600

    JSON API database requests

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,11 +2,16 @@ import React from "react";
 
 const Home = () => {
   return (
+    <div>
+    <picture>
+          <img src={require("./home.png")} alt="Happy Puppy" />
+    </picture>
     <address>
       Visit Us at the Nashville North Location
       <br />
       500 Puppy Way
     </address>
+    </div>
   );
 };
 
