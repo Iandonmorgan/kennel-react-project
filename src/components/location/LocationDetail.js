06ecf36da40858ceeafe2f03e4b7241a02004690
Landon Morgan <morganlandon@gmail.com>
2020-02-26 12:05:47 -0600

    chapter 7 completed, practice: close a location

--- src/components/location/LocationDetail.js
+++ src/components/location/LocationDetail.js
@@ -3,30 +3,43 @@ import LocationManager from '../../modules/LocationManager';
 import './LocationDetail.css'
 
 const LocationDetail = props => {
-  const [location, setLocation] = useState({ name: "", address: "" });
+    const [location, setLocation] = useState({ name: "", address: "" });
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    //get(id) from LocationManager and hang on to the data; put it into state
-    LocationManager.get(props.locationId)
-      .then(location => {
-        setLocation({
-          name: location.name,
-          address: location.address
-        });
-      });
-  }, [props.locationId]);
+    const handleDelete = () => {
+        //invoke the delete function in LocationManager and re-direct to the location list.
+        setIsLoading(true);
+        LocationManager.delete(props.locationId).then(() =>
+            props.history.push("/locations")
+        );
+    };
 
-  return (
-    <div className="card">
-      <div className="card-content">
-        <picture>
-        <img src={require("./loc.png")} alt="Location" />
-        </picture>
-        <h3>Name: <span style={{ color: 'darkslategrey' }}>{location.name}</span></h3>
-        <p>Address: {location.address}</p>
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        //get(id) from LocationManager and hang on to the data; put it into state
+        LocationManager.get(props.locationId)
+            .then(location => {
+                setLocation({
+                    name: location.name,
+                    address: location.address
+                });
+                setIsLoading(false);
+            });
+    }, [props.locationId]);
+
+    return (
+        <div className="card">
+            <div className="card-content">
+                <picture>
+                    <img src={require("./loc.png")} alt="Location" />
+                </picture>
+                <h3>Name: <span style={{ color: 'darkslategrey' }}>{location.name}</span></h3>
+                <p>Address: {location.address}</p>
+                <button type="button" disabled={isLoading} onClick={handleDelete}>
+                    Shutter Location
+        </button>
+            </div>
+        </div>
+    );
 }
 
 export default LocationDetail;
