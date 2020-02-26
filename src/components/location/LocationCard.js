b60a5a0458391bb4cb14fe10816583fb99263614
Landon Morgan <morganlandon@gmail.com>
2020-02-26 11:57:13 -0600

    chapter 6 practice, show location details complete

--- src/components/location/LocationCard.js
+++ src/components/location/LocationCard.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./Location.css"
+import "./Location.css";
+import { Link } from "react-router-dom";
 
 const LocationCard = (props) => {
   return (
@@ -12,6 +13,9 @@ const LocationCard = (props) => {
           Name: <span className="card-locname">{props.location.name}</span>
         </h3>
         <p>Address: {props.location.address}</p>
+        <Link to={`/locations/${props.location.id}`}>
+          <button>Details</button>
+        </Link>
         <button type="button" onClick={() => props.deleteLocation(props.location.id)}>Shutter Location</button>
       </div>
     </div>
