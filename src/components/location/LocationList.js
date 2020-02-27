f67648b927f6c7ae108eb7b88180aa6f86d193bd
Landon Morgan <morganlandon@gmail.com>
2020-02-27 12:04:23 -0600

    adding objects with forms completed - chapter 9

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -3,38 +3,47 @@ import React, { useState, useEffect } from 'react';
 import LocationCard from './LocationCard';
 import LocationManager from '../../modules/LocationManager';
 
-const LocationList = () => {
-  // The initial state is an empty array
-  const [locations, setLocations] = useState([]);
+const LocationList = (props) => {
+    // The initial state is an empty array
+    const [locations, setLocations] = useState([]);
 
-  const deleteLocation = id => {
-    LocationManager.delete(id)
-      .then(() => LocationManager.getAll().then(setLocations));
-  };
+    const deleteLocation = id => {
+        LocationManager.delete(id)
+            .then(() => LocationManager.getAll().then(setLocations));
+    };
 
-  const getLocations = () => {
-    // After the data comes back from the API, we
-    //  use the setLocations function to update state
-    return LocationManager.getAll().then(locationsFromAPI => {
-      setLocations(locationsFromAPI)
-    });
-  };
+    const getLocations = () => {
+        // After the data comes back from the API, we
+        //  use the setLocations function to update state
+        return LocationManager.getAll().then(locationsFromAPI => {
+            setLocations(locationsFromAPI)
+        });
+    };
 
-  // got the locations from the API on the component's first render
-  useEffect(() => {
-    getLocations();
-  }, []);
+    // got the locations from the API on the component's first render
+    useEffect(() => {
+        getLocations();
+    }, []);
 
-  // Finally we use map() to "loop over" the locations array to show a list of location cards
-  return(
-    <div className="container-cards">
-      {locations.map(location =>
-        <LocationCard
-        key={location.id}
-        location={location}
-        deleteLocation={deleteLocation} />
-      )}
-    </div>
-  );
+    // Finally we use map() to "loop over" the locations array to show a list of location cards
+    return (
+        <div>
+            <section className="section-content">
+                <button type="button"
+                    className="btn"
+                    onClick={() => { props.history.push("/locations/new") }}>
+                    Add Location
+                </button>
+            </section>
+            <div className="container-cards">
+                {locations.map(location =>
+                    <LocationCard
+                        key={location.id}
+                        location={location}
+                        deleteLocation={deleteLocation} />
+                )}
+            </div>
+        </div>
+    );
 };
 export default LocationList
